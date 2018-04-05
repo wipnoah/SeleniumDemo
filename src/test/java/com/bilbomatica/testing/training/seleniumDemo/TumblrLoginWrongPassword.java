32bc4206c068d33d875c49de1cde6ad9f37eaec9
jpf <wipnoah@gmail.com>
2018-04-05 15:17:41 +0200

    initial commit

--- src/test/java/com/bilbomatica/testing/training/seleniumDemo/TumblrLoginWrongPassword.java
+++ src/test/java/com/bilbomatica/testing/training/seleniumDemo/TumblrLoginWrongPassword.java
@@ -1,21 +1,24 @@
 package com.bilbomatica.testing.training.seleniumDemo;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.Assert.*;
 import org.openqa.selenium.By;
 
-public class TumblrLoginTest extends  BaseLocalRemoteWebDriverTest{
+import static org.testng.Assert.assertTrue;
+
+public class TumblrLoginWrongPassword extends BaseLocalRemoteWebDriverTest{
+
 
     @Test
-    public void testLogin(){
+    public void testLoginWrongPassword(){
         driver.get("https://www.tumblr.com/");
         driver.findElement(By.id("signup_login_button")).click();
         driver.findElement(By.id("signup_determine_email")).sendKeys("wipnoah@gmail.com");
         driver.findElement(By.id("signup_forms_submit")).click();
-         driver.findElement(By.id("signup_password")).sendKeys("blackdragon");
+        driver.findElement(By.id("signup_password")).sendKeys("wrongpassword");
         driver.findElement(By.id("signup_forms_submit")).click();
-        Assert.assertTrue(driver.findElementById("Compose-button")!= null);
+        assertTrue(driver.findElement(By.cssSelector("BODY")).getText().contains("Your email or password were incorrect."));
 
     }
+
 }
